6bae1542a5b1266fdd582d523717f8d9f73a138c
elenagizikova <gizikovaelena@gmail.com>
2019-11-13 00:28:56 -0800

    Automate Login scenarios
    Homework: Finish Automation tests for Login
    Ticket 2. Automate Login scenarios

--- src/test/java/LoginTests.java
+++ src/test/java/LoginTests.java
@@ -5,26 +5,66 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
+import java.util.concurrent.TimeUnit;
+
 public class LoginTests {
 
+
     @Test
-    public static void SignUpShortPassword () {
+    public static void loginSuccessTest()
 
 
-        //This test is to verify error message for password less than 8 characters
-        WebDriver driver = new ChromeDriver ();
-        driver.get ("https://deens-master.now.sh/register" );
-        WebElement login_button = driver.findElement ( By.cssSelector ( "[id=\"username\"]" ));
-        login_button.click ();
-        login_button.sendKeys ( "elena" );
-        WebElement email = driver.findElement ( By.cssSelector ( "[id=\"email\"]" ) );
-        email.sendKeys ( "gizikovaelena@gmail.com" );
-        WebElement password = driver.findElement ( By.cssSelector ( "[id=\"password\"]" ) );
-        password.sendKeys ( "Test123" );
-        WebElement register = driver.findElement ( By.cssSelector ( "[class=\"ui large fluid button green-btn pl-btn\"]" ) );
-        register.click ();
-        WebElement uiErrorMessage = driver.findElement ( By.cssSelector ( "[class=\"ui message\"]" ) );
-        String errorMessage = uiErrorMessage.getText ();
-        Assert.assertEquals (errorMessage, "Password must be at least 8 characters long" );
+    {
+        WebDriver driver = new ChromeDriver();
+
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.get("https://deens.com/login");
+        driver.findElement(By.cssSelector("#email")).click();
+        driver.findElement(By.cssSelector("#email")).sendKeys("macarena1@gmail.com");
+        driver.findElement(By.cssSelector("#password")).click();
+        driver.findElement(By.cssSelector("#password")).sendKeys("Test123Test123");
+        driver.findElement(By.cssSelector(".ui.large.fluid.button.green-btn.pl-btn")).click();
+        Assert.assertTrue (driver.findElement ( By.cssSelector ("[class*='DesktopDropDownMenu__AvatarWrapper']") ).isDisplayed ());
+        // driver.quit();
     }
-}
+
+
+    @Test
+    public static void emptyCredentials() {
+
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://deens.com/login");
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        WebElement login_button = driver.findElement(By.cssSelector("[data-testid=\"loginSubmit\"]"));
+        login_button.click();
+        WebElement uiErrorMessage = driver.findElement(By.cssSelector(".ui.error.message"));
+        String errorMessage = uiErrorMessage.getText();
+        Assert.assertEquals(errorMessage, "Cannot login\n" + "Empty email or password");
+        // driver.quit();
+
+    }
+
+    @Test
+    public static void wrongCredentials() {
+
+        WebDriver driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.get ( "https://deens.com/login" );
+        WebElement email_button = driver.findElement ( By.cssSelector ("#email"));
+        email_button.sendKeys ( "ajgirre@gmail.com" );
+        WebElement incorrectpassword_button = driver.findElement ( By.cssSelector ("#password"));
+        incorrectpassword_button.sendKeys ( "jhhfjdkfhd" );
+        WebElement login_button = driver.findElement (By.cssSelector ("[data-testid=\"loginSubmit\"]"));
+        login_button.click ( );
+        WebElement uiErrorMessage = driver.findElement ( By.cssSelector (".ui.error.message"));
+        String errorMessage = uiErrorMessage.getText ();
+        Assert.assertEquals ( errorMessage, "Cannot login\n" + "no user found with email: ajgirre@gmail.com" );
+        // driver.quit();
+
+
+    }
+    }
+
+
+
+

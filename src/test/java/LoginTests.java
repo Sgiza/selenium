0fea71343d8684a2a67ce5455998d89bbb9cd3ff
elenagizikova <gizikovaelena@gmail.com>
2019-11-18 21:31:43 -0800

    Update LoginTests.java
    Updated LoginTests file

--- src/test/java/LoginTests.java
+++ src/test/java/LoginTests.java
@@ -3,68 +3,75 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
-import org.testng.annotations.Test;
+import org.testng.annotations.*;
 
 import java.util.concurrent.TimeUnit;
 
-public class LoginTests {
+public class LoginTests extends BaseTest {
 
-
-    @Test
-    public static void loginSuccessTest()
-
-
-    {
-        WebDriver driver = new ChromeDriver();
-
-        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-        driver.get("https://deens.com/login");
-        driver.findElement(By.cssSelector("#email")).click();
-        driver.findElement(By.cssSelector("#email")).sendKeys("macarena1@gmail.com");
-        driver.findElement(By.cssSelector("#password")).click();
-        driver.findElement(By.cssSelector("#password")).sendKeys("Test123Test123");
-        driver.findElement(By.cssSelector(".ui.large.fluid.button.green-btn.pl-btn")).click();
-        Assert.assertTrue (driver.findElement ( By.cssSelector ("[class*='DesktopDropDownMenu__AvatarWrapper']") ).isDisplayed ());
-        // driver.quit();
-    }
-
-
-    @Test
-    public static void emptyCredentials() {
-
-        WebDriver driver = new ChromeDriver();
-        driver.get("https://deens.com/login");
-        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-        WebElement login_button = driver.findElement(By.cssSelector("[data-testid=\"loginSubmit\"]"));
-        login_button.click();
-        WebElement uiErrorMessage = driver.findElement(By.cssSelector(".ui.error.message"));
-        String errorMessage = uiErrorMessage.getText();
-        Assert.assertEquals(errorMessage, "Cannot login\n" + "Empty email or password");
-        // driver.quit();
-
-    }
-
-    @Test
-    public static void wrongCredentials() {
-
-        WebDriver driver = new ChromeDriver();
-        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+    @BeforeMethod
+    public void openLoginPage () {
         driver.get ( "https://deens.com/login" );
-        WebElement email_button = driver.findElement ( By.cssSelector ("#email"));
-        email_button.sendKeys ( "ajgirre@gmail.com" );
-        WebElement incorrectpassword_button = driver.findElement ( By.cssSelector ("#password"));
-        incorrectpassword_button.sendKeys ( "jhhfjdkfhd" );
-        WebElement login_button = driver.findElement (By.cssSelector ("[data-testid=\"loginSubmit\"]"));
-        login_button.click ( );
-        WebElement uiErrorMessage = driver.findElement ( By.cssSelector (".ui.error.message"));
-        String errorMessage = uiErrorMessage.getText ();
-        Assert.assertEquals ( errorMessage, "Cannot login\n" + "no user found with email: ajgirre@gmail.com" );
-        // driver.quit();
+
+    }
+
+    @Test
+    public void loginSuccessTest () {
+        enterEmail ( "macarena1@gmail.com" );
+        enterPassword ( "Test123Test123" );
+        submit ( );
+        Assert.assertTrue ( avatarAppeared ( ) );
+
+    }
+
+    @Test
+    public void emptyCredentials () {
+
+        submit ( );
+        Assert.assertEquals ( getErrorMessage (), "Cannot login\n" + "Empty email or password" );
+
+    }
+
+    @Test
+    public void wrongCredentials () {
+
+
+        enterEmail ( "ajgirre@gmail.com" );
+        enterPassword ( "hfdgfdkghkfdl" );
+        submit ( );
+        Assert.assertEquals ( getErrorMessage (), "Cannot login\n" + "no user found with email: ajgirre@gmail.com" );
 
 
     }
+
+    public void enterPassword ( String password ) {
+        driver.findElement ( By.cssSelector ( "#password" ) ).sendKeys ( password );
+    }
+
+    public void enterEmail ( String email ) {
+        driver.findElement ( By.cssSelector ( "#email" ) ).sendKeys ( email );
     }
 
 
+    public void submit () {
+        driver.findElement ( By.cssSelector ( "[data-testid=\"loginSubmit\"]" ) ).click ( );
+    }
+
+    public boolean avatarAppeared () {
+
+        return driver.findElement ( By.cssSelector ( "[class*='DesktopDropDownMenu__AvatarWrapper']" ) ).isDisplayed ( );
+    }
+
+    private String getErrorMessage () {
+        WebElement uiErrorMessage = findElement ( ".ui.error.message" );
+        String errorMessage = uiErrorMessage.getText ( );
+        return errorMessage;
+    }
+
+     public WebElement findElement (String cssLocator) {
+
+         return driver.findElement ( By.cssSelector (cssLocator ) );
+     }
+}
 
 
